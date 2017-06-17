57649799622c3d3f069fd4c07cde44d2ffddefe7
SKowshik4614 <santosh.kowshik20@gmail.com>
2017-06-18 02:46:32 +0530

    Add files via upload

---
+++ data_filter.php
@@ -0,0 +1,71 @@
+<?php
+
+if(isset($_POST['search']))
+{
+    $valueToSearch = $_POST['valueToSearch'];
+    // search in all table columns
+    // using concat mysql function
+    $query = "SELECT * FROM `doctors_data` WHERE CONCAT(`Doctor_Name`, `Hospital_Name`, `Qualification`, `Location`, `Last_Updated_Date`) LIKE '%".$valueToSearch."%'";
+    $search_result = filterTable($query);
+    
+}
+ else {
+    $query = "SELECT * FROM `doctors_data`";
+    $search_result = filterTable($query);
+}
+
+// function to connect and execute the query
+function filterTable($query)
+{
+    $connect = mysqli_connect("localhost", "root", "Revenant.2092", "santosh");
+    $filter_Result = mysqli_query($connect, $query);
+    return $filter_Result;
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Medinfi Healthcare Pvt Ltd </title>
+		<link rel="shortcut icon" href="/images/title.png" type="image/png">
+        <style>
+            table,tr,th,td
+            {
+                border: 1px solid black;
+            }
+        </style>
+    </head>
+    <body background="/images/backgrod_filter.png">
+        <p>
+			<h1 align="center" style="font-size:300%;"> <img src="/images/logo.png" alt="logo" style="width:40px;height:40px;"/>Medinfi Healthcare Pvt Ltd </h1>
+		</p>
+        <form action="data_filter.php" method="post">
+			<p>
+				<b>Enter a keyword to search:</b> <input type="text" name="valueToSearch" placeholder="Value To Search">
+				<input type="submit" name="search" value="Filter" style="color: black;"><br><br>
+            </p>
+            <table>
+                <tr>
+                    <th bgcolor="yellow">Doctor_Name</th>
+                    <th bgcolor="yellow">Hospital_Name</th>
+                    <th bgcolor="yellow">Qualification</th>
+                    <th bgcolor="yellow">Location</th>
+					<th bgcolor="yellow">Last_Updated_Date</th>
+                </tr>
+
+      <!-- populate table from mysql database -->
+                <?php while($row = mysqli_fetch_array($search_result)):?>
+                <tr>
+                    <td align="left"><?php echo $row['Doctor_Name'];?></td>
+                    <td align="left"><?php echo $row['Hospital_Name'];?></td>
+                    <td align="left"><?php echo $row['Qualification'];?></td>
+                    <td align="left"><?php echo $row['Location'];?></td>
+					<td align="left"><?php echo $row['Last_Updated_Date'];?></td>
+                </tr>
+                <?php endwhile;?>
+            </table>
+        </form>
+        
+    </body>
+</html>
---
+++ data_last_30days.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once('../mysql_conn.php');
+
+$query = "SELECT Doctor_Name, Hospital_Name, Qualification, Location, Last_Updated_Date from doctors_data where Last_Updated_Date BETWEEN DATE_SUB(NOW(), INTERVAL 30 DAY) AND NOW() order by Doctor_Name,Hospital_Name,Location";
+
+$response = @mysqli_query($dbc,$query);
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Medinfi Healthcare Pvt Ltd </title>
+		<link rel="shortcut icon" href="/images/title.png" type="image/png">
+        <style>
+            table,tr,th,td
+            {
+                border: 1px solid black;
+            }
+        </style>
+    </head>
+    <body background="/images/backgrod_filter.png">
+        <p>
+			<h1 align="center" style="font-size:300%;"> <img src="/images/logo.png" alt="logo" style="width:40px;height:40px;"/>Medinfi Healthcare Pvt Ltd </h1>
+		</p>
+            <table align="center">
+                <tr>
+                    <th bgcolor="yellow">Doctor_Name</th>
+                    <th bgcolor="yellow">Hospital_Name</th>
+                    <th bgcolor="yellow">Qualification</th>
+                    <th bgcolor="yellow">Location</th>
+					<th bgcolor="yellow">Last_Updated_Date</th>
+                </tr>
+
+      <!-- populate table from mysql database -->
+                <?php while($row = mysqli_fetch_array($response)):?>
+                <tr>
+                    <td align="left"><?php echo $row['Doctor_Name'];?></td>
+                    <td align="left"><?php echo $row['Hospital_Name'];?></td>
+                    <td align="left"><?php echo $row['Qualification'];?></td>
+                    <td align="left"><?php echo $row['Location'];?></td>
+					<td align="left"><?php echo $row['Last_Updated_Date'];?></td>
+                </tr>
+                <?php endwhile;?>
+            </table>
+        </form>
+        
+    </body>
+</html>
